425b43e17c48f9376195835e4176337ff6b4b697
Irene Tello Arista <irene.tello.arista@gmail.com>
2023-11-08 17:43:26 +0100

    Update Assignment 1.r

--- Assignment 1/Assignment 1.r
+++ Assignment 1/Assignment 1.r
@@ -2,3 +2,6 @@
 #Irene Tello Arista, 
 #First assignment, Prediction with Machine Learning for Economists 2023/24 Fall
 #########################################################################################
+#Set working directory
+
+#Upload the dataset 
